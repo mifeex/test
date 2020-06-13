86e898441916e3d75485cef07b23cfa2efc4bf88
Evgeniy <60233967+mifeex@users.noreply.github.com>
2020-06-14 01:36:34 +0400

    build with test app

---
+++ build/precache-manifest.b0d2caba3426e5aeb0108c8ec6586114.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2288c1761e0bd16c01f982d6a6b39bbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df4e08b776e8bcd2b091",
+    "url": "/static/css/main.90f6c1a6.chunk.css"
+  },
+  {
+    "revision": "78667c00c62f60f3421f",
+    "url": "/static/js/2.d2b01a5e.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/static/js/2.d2b01a5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4e08b776e8bcd2b091",
+    "url": "/static/js/main.c9278fa2.chunk.js"
+  },
+  {
+    "revision": "b9833fc31dc5d45f4568",
+    "url": "/static/js/runtime-main.f2719cf8.js"
+  }
+]);
